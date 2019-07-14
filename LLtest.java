eff627696841b23b7640633e65f728aece2c928e
SidharthSunil <47484513+SidharthSunil@users.noreply.github.com>
2019-07-14 22:59:17 +0530

    main program to use linked list features created

---
+++ LLtest.java
@@ -0,0 +1,18 @@
+//main class to use the features of the linked list class created
+
+public class LLtest {
+	public static void main(String[] args) {
+		/*Linkedlist l = new Linkedlist();
+		l.insert(5);
+		l.insert(6);
+		l.insertbeg(2);
+		l.insert(7);
+		l.insert(20);
+		l.insert(11);
+		l.insert(30);
+		l.insertat(4, 23);
+		l.delete(7);
+		l.middle();
+		l.print();*/
+	}
+}
